91fa7016a806dc4fd8bed27876821e219237c213
money <lucasphone75@gmail.com>
2024-01-12 16:40:42 -0700

    Script for all consensus variants

--- scripts/update.sh
+++ scripts/update.sh
@@ -1,55 +1,93 @@
 #!/bin/bash
 
-cd /local/rs-paxos
+cd /local/etcd
 cd .. & git clone https://github.com/Bompedy/RS-Paxos.git
 
 git config --global --add safe.directory /local/etcd/ETCD
 git config --global --add safe.directory /local/etcd/PineappleGo
 git config --global --add safe.directory /local/etcd/RabiaGo
-git config --global --add safe.directory /local/etcd/RS-Paxos.git
+git config --global --add safe.directory /local/etcd/RS-Paxos
 
-cd RS-Paxos
+cd ETCD
+cd ../RS-Paxos || exit
 git stash && git stash clear && git pull
-cd ../ETCD
+cd ../Raft || exit
 git stash && git stash clear && git pull
+cd ../RabiaGo || exit
+git stash && git stash clear && git pull
+cd ../PineappleGo || exit
+git stash && git stash clear && git pull
+cd ../ETCD || exit
+git stash && git stash clear && git pull
+
+if [ "$#" -lt 1 ]; then
+  echo "Usage: $0 <nodes> [raft|rs-rabia|rs-paxos|pineapple [memory]]"
+  exit 1
+fi
+
+nodes=$1
 
 HOST=$(hostname | awk -F "." '{print $1}')
 echo "Hostname: $HOST"
+for ((i = 1; i <= nodes; i++)); do
+  if [ "$HOST" = "node-$i" ]; then
+    IP="10.10.1.$i"
+    break
+  fi
+done
 
-
-if [ $HOST = "node-1" ]; then
-    IP="10.10.1.1"
-elif [ $HOST = "node-2" ]; then
-    IP="10.10.1.2"
-elif [ $HOST = "node-3" ]; then
-    IP="10.10.1.3"
-elif [ $HOST = "node-4" ]; then
-    IP="10.10.1.4"
-elif [ $HOST = "node-5" ]; then
-    IP="10.10.1.5"
-fi
 echo "Local IP: $IP"
 
-#if [ "$1" = "raft" ]; then
-  export PINEAPPLE="false"
-  export RS_PAXOS="true"
-#export SETUP="--initial-cluster node-1=http://10.10.1.1:12380,node-2=http://10.10.1.2:12380,node-3=http://10.10.1.3:12380,node-4=http://10.10.1.4:12380,node-5=http://10.10.1.5:12380"
-#elif [ "$1" = "paxos" ]; then
-#  export RS_PAXOS="true"
-#else
-#  export PINEAPPLE="true"
-#fi
 
-#process_info=$(ps aux | grep "$script_name" | grep -v grep)
-#if [ -n "$process_info" ]; then
-#  echo "Process is running: $process_info"
-#else
-#  echo "Process is not running."
-#fi
+export RS_RABIA="false"
+export RS_PAXOS="false"
+export PINEAPPLE="false"
+export PINEAPPLE_MEMORY="false"
+export PARITY = 0
+export SEGMENTS = 0
+
+if [ "$2" = "rs-rabia" ]; then
+  export RS_RABIA="true"
+    if [ "$#" -ge 4 ]; then
+      PARITY=$3
+      SEGMENTS=$4
+    else
+      echo "Please provide PARITY and SEGMENTS arguments for 'rs-rabia'."
+      exit 1
+    fi
+elif [ "$2" = "rs-paxos" ]; then
+  export RS_PAXOS="true"
+    if [ "$#" -ge 4 ]; then
+      PARITY=$3
+      SEGMENTS=$4
+    else
+      echo "Please provide PARITY and SEGMENTS arguments for 'rs-paxos'."
+      exit 1
+    fi
+elif [ "$2" = "raft" ]; then
+  export SETUP="--initial-cluster "
+    for ((i = 1; i <= nodes; i++)); do
+      export SETUP+="node-$i=http://10.10.1.$i:12380"
+      if [ "$i" -lt "$nodes" ]; then
+        export SETUP+=","
+      fi
+    done
+    echo "SETUP: $SETUP"
+else
+  if [ "$2" = "pineapple" ]; then
+    if [ "$3" = "memory" ]; then
+      export PINEAPPLE="true"
+      export PINEAPPLE_MEMORY="true"
+    export PINEAPPLE="true"
+    fi
+  else
+    echo "Invalid argument. Please use 'rs-rabia', 'rs-paxos', 'pineapple', or 'pineapple memory'."
+    exit 1
+  fi
+fi
 
 sudo rm -rf "$HOST.etcd"
 make build
-pwd
 sudo ./bin/etcd --log-level panic \
 --name "$HOST" \
 --initial-cluster-token etcd-cluster-1 \
@@ -57,8 +95,11 @@ sudo ./bin/etcd --log-level panic \
 --advertise-client-urls http://"$IP":2379 \
 --initial-advertise-peer-urls http://"$IP":12380 \
 --listen-peer-urls http://"$IP":12380 \
---quota-backend-bytes 20000000000 \
+--quota-backend-bytes 10000000000 \
 --snapshot-count 0 \
---max-request-bytes 104857600
+--max-request-bytes 104857600 \
 $SETUP \
 --initial-cluster-state new
+
+
+
